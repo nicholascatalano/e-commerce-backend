900c1d0a53ab84048b3da2117b72d05ce1f5bd88
nickcatalano5 <nickcatalano24@gmail.com>
2024-01-08 17:06:48 -0500

    adds async get route to findOne Product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -28,9 +28,30 @@ router.get("/", async (req, res) => {
 });
 
 // get one product
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findOne({
+      where: {
+        id: req.params.id,
+      },
+      attributes: ["id", "product_name", "price", "stock", "category_id"],
+      include: [
+        {
+          model: Category,
+          attributes: ["id", "category_name"],
+        },
+        {
+          model: Tag,
+          attributes: ["id", "tag_name"],
+        },
+      ],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
