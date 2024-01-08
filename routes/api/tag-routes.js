f48bf0a0970c94b11c6288c86248d2c3406f95a7
nickcatalano5 <nickcatalano24@gmail.com>
2024-01-08 17:13:47 -0500

    adds post route to create a Tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -44,8 +44,17 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create({
+      id: req.body.id,
+      tag_name: req.body.tag_name,
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put("/:id", (req, res) => {
