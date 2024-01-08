d08681b26a040c421aed06608dc63ff0c3f41fa1
nickcatalano5 <nickcatalano24@gmail.com>
2024-01-08 16:49:19 -0500

    adds Products belongsTo Category relationship

--- models/index.js
+++ models/index.js
@@ -1,10 +1,13 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
 // Categories have many Products
 
